3d7949796055539931f2deaaba1f007aad17a096
teak1 <trainian@gmail.com>
2018-11-30 00:23:41 -0800

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,12 @@ CR.on(CR.hooks.pre_render, function (context) {
     context.rect(context.width / 2, context.height / 2, context.width, context.height, context.Color("black"), context.Color("black"));
 })
 CR.on(CR.hooks.render, function (context) {
-    // for (var i = 0; i < 80; i++) {
-    //     context.rect(context.width * Math.random(), context.height * Math.random(), 15, 15, context.Color("red"), context.Color(0, 0, 0, 255), new Date().getTime() / 100);
-    // }
+    for (var j = 0; j < 20; j++) {
+        for (var i = 0; i < 20; i++) {
+            context.rect(i * 45 + 25, 25 + j * 45, 15, 45, context.Color("red"), context.Color("black"), new Date().getTime() / 10 + i * j);
+            context.rect(i * 45 + 25, 30 + j * 45, 15, 45, context.Color("red"), context.Color("black"), new Date().getTime() / 10 + i * j);
+        }
+        // debugger;
+    }
+    // context.rect(100 + 80, 80, 15, 15, context.Color("red"), context.Color(0, 0, 0, 255), new Date().getTime() / 100);
 });
